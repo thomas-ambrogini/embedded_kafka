2f7871c0824549ca7afbcef4d2e1ca495efc00bf
thomas <thomas@Ubuntu.myguest.virtualbox.org>
2023-09-05 10:14:51 +0200

    chore: create_topics_file.sh script makes the conf directory if it doesn't exist

--- scripts/create_topics_file.sh
+++ scripts/create_topics_file.sh
@@ -2,6 +2,8 @@
 
 # Create an array to store the input strings as objects
 json_array=()
+conf_directory="build/conf"
+json_file="$conf_directory/topics.json"
 
 # Iterate through the command-line arguments and add them to the array
 for ((i=1; i<=$#; i++)); do
@@ -13,9 +15,10 @@ done
 
 json_data=$(IFS=, ; echo "${json_array[*]}")
 
+if [ ! -d "$conf_directory" ]; then
+    mkdir -p "$conf_directory"
+fi
 
-# Create the JSON file
-json_file="build/conf/topics.json"
 echo "[$json_data]" > "$json_file"
 
 echo "JSON file '$json_file' created with the following content:"
